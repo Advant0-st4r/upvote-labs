5743af01c9efc685449cd8faff887d9d0ac6d5f4
Advant0-st4r <advantoincorporated@gmail.com>
2025-10-02 16:14:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import path from 'path';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    target: 'esnext',
+  server: {
+    port: 3000,
   },
 });
+
